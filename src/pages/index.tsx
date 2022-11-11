366abc275f027777c8f2a3d89cc1fe51ff18f059
Arjun <arjun140702@gmail.com>
2022-11-12 01:35:06 +0530

    Add a dark mode toggle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
-import { Center, Heading, VStack } from "@chakra-ui/react"
+import { Center, Heading, VStack, HStack, Spacer } from "@chakra-ui/react"
 import type { NextPage } from "next"
+import ColorToggle from "../components/ColorToggle"
 import Head from "../components/Head"
 import UploadButton from "../components/UploadButton"
 
 const Home: NextPage = () => {
     return (
         <Center minH="100vh">
-            <VStack p={8} spacing={4}>
+            <VStack p={8} spacing={8}>
                 <Head />
-                <Heading>SQLi Fix for PHP</Heading>
+                <HStack justifyContent="center" alignItems="center" w="100%">
+                    <Heading>SQLi Fix for PHP</Heading>
+                    <Spacer />
+                    <ColorToggle />
+                </HStack>
                 <UploadButton />
             </VStack>
         </Center>
